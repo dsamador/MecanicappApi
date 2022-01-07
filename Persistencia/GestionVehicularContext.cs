888de6c5016631000ffe803ff8fe575c787dfbab
David Amador <54488581+dsamador@users.noreply.github.com>
2022-01-07 11:55:04 -0500

    Mapeando EF

--- Persistencia/GestionVehicularContext.cs
+++ Persistencia/GestionVehicularContext.cs
@@ -1,8 +1,35 @@
 
+using Dominio;
+using Microsoft.EntityFrameworkCore;
+
 namespace Persistencia
 {
     public class GestionVehicularContext : DbContext
     {
-        
+        public GestionVehicularContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        /* Metodo que viene de DbContext y estamos sobreescribiendo */
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {   
+        }
+
+        /* ######################## */
+        /* Mapeando las clases a EF */
+        /* ######################## */
+        public DbSet<Gasolinera> Gasolinera { get; set; }
+        public DbSet<Lavadero> Lavadero { get; set; }
+        public DbSet<Lavado> Lavado { get; set; }
+        public DbSet<Mantenimiento> Mantenimiento { get; set; }
+        public DbSet<MarcaVehiculo> MarcaVehiculo { get; set; }
+        public DbSet<RecargaCombustible> RecargaCombustible { get; set; }
+        public DbSet<Taller> Taller { get; set; }
+        public DbSet<TipoCombustible> TipoCombustible { get; set; }
+        public DbSet<TipoLavado> TipoLavado { get; set; }
+        public DbSet<TipoMantenimiento> TipoMantenimiento { get; set; }
+        public DbSet<TipoVehiculo> TipoVehiculo { get; set; }
+        public DbSet<Vehiculo> Vehiculo { get; set; }
     }
 }
